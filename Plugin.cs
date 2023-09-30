bcc24226f059f246621e572d63dcbbd8d5daddf4
Shade <martiangames@gmail.com>
2023-09-30 10:52:50 -0700

    Init

---
+++ Plugin.cs
@@ -0,0 +1,56 @@
+﻿using Jacobi.Vst.Core;
+using Jacobi.Vst.Framework;
+using Jacobi.Vst.Framework.Plugin;
+using System;
+
+namespace TheoSynth
+{
+    public class Plugin : VstPluginBase
+    {
+        private readonly SineSynth _sineSynth;
+
+        public Plugin()
+            : base("Simple Sine Synth", new VstProductInfo("Simple Sine", "MyCompany", 1000), VstPluginFlags.NoFlags)
+        {
+            _sineSynth = new SineSynth();
+        }
+
+        public override IVstPluginAudioProcessor AudioProcessor => _sineSynth;
+        public override IVstMidiProcessor MidiProcessor => _sineSynth;
+
+
+
+        public Plugin() : base("Simple Sine Synth", new VstProductInfo("Simple Sine", "MyCompany", 1000), VstPluginFlags.NoFlags)
+        {
+            _sineSynth = new SineSynth();
+            Parameters.Add(new VstParameterInfo
+            {
+                CanBeAutomated = true,
+                Name = "Gain",
+                ShortLabel = "Gain",
+                MinValue = 0.0f,
+                MaxValue = 1.0f,
+                DefaultValue = 0.5f,
+                LargeStepFloat = 0.1f,
+                SmallStepFloat = 0.01f,
+                StepFloat = 0.05f,
+                Label = string.Empty,
+                DisplayIndex = 0,
+                Category = new VstParameterCategory(),
+                CanRamp = true
+            });
+        }
+
+
+        public override void ProcessParameterValueChange(VstParameterChangeEventArgs e)
+        {
+            base.ProcessParameterValueChange(e);
+            if (e.Parameter.Name == "Gain")
+            {
+                _sineSynth.Gain = e.NewValue;
+            }
+        }
+        
+
+    }
+}
---
+++ SineSynth.cs
@@ -0,0 +1,89 @@
+﻿using Jacobi.Vst.Core;
+using Jacobi.Vst.Framework;
+using System;
+
+namespace TheoSynth
+{
+    public class SineSynth : IVstPluginAudioProcessor, IVstMidiProcessor
+    {
+        private double _phase;
+        private double _frequency = 440; // Default to A4 note
+        private readonly double _twoPi = 2 * Math.PI;
+        private bool _noteOn;
+        public int SampleRate { get; private set; } = 44100; // Default to 44.1kHz
+        public float Gain { get; set; } = 0.5f; // default gain
+
+        // Define proper channel counts
+        int IVstPluginAudioProcessor.ChannelCount => 2; // stereo audio channel
+        int IVstMidiProcessor.ChannelCount => 1; // mono MIDI channel
+
+        public void Process(VstAudioBuffer[] inChannels, VstAudioBuffer[] outChannels)
+        {
+            if (_noteOn)
+            {
+                for (int i = 0; i < outChannels[0].SampleCount; i++)
+                {
+                    double sample = Math.Sin(_phase) * Gain;
+                    foreach (var outChannel in outChannels)
+                    {
+                        outChannel[i] = (float)sample;
+                    }
+
+                    _phase += _twoPi * _frequency / SampleRate;
+                    if (_phase >= _twoPi) _phase -= _twoPi;
+                }
+            }
+        }
+
+        public void Process(VstEventCollection events)
+        {
+            foreach (VstEvent ev in events)
+            {
+                if (ev.EventType == VstEventTypes.MidiEvent)
+                {
+                    VstMidiEvent midiEvent = (VstMidiEvent)ev;
+                    byte status = midiEvent.Data[0];
+                    byte midiNote = midiEvent.Data[1];
+
+                    if ((status & 0xF0) == 0x90) // note on
+                    {
+                        NoteOn(midiNote);
+                    }
+                    else if ((status & 0xF0) == 0x80) // note off
+                    {
+                        NoteOff();
+                    }
+                }
+            }
+        }
+
+        public void NoteOn(int midiNoteNumber)
+        {
+            _frequency = 440.0 * Math.Pow(2, (midiNoteNumber - 69) / 12.0);
+            _noteOn = true;
+        }
+
+        public void NoteOff()
+        {
+            _noteOn = false;
+        }
+
+        // Implement other required methods, but you can leave them empty
+        public bool SupportsSampleRate(int sampleRate) => true;
+
+        public bool SetSampleRate(int sampleRate)
+        {
+            SampleRate = sampleRate;
+            return true;
+        }
+
+        public void Start() { }
+        public void Stop() { }
+
+        public bool ProcessReplacing(VstAudioBuffer[] inChannels, VstAudioBuffer[] outChannels)
+        {
+            Process(inChannels, outChannels);
+            return true;
+        }
+    }
+}
